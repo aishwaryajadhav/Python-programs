21ec5a8df94bfadb991d0928840595d3a72dd827
aishwaryajadhav <me.aishwaryajadhav@gmail.com>
2016-11-06 15:27:12 +0530

    Add files via upload

---
+++ remote procedure call/rpcclient.py
@@ -0,0 +1,12 @@
+#Remote procedure call client
+
+import xmlrpclib
+
+
+a=int(input('Enter 1st number'))
+
+b=int(input('Enter 2nd number'))
+
+proxy=xmlrpclib.ServerProxy('http://172.18.39.132:9000')
+    #Enter server ip and port number
+print proxy.add(a,b)
---
+++ remote procedure call/rpcserver.py
@@ -0,0 +1,32 @@
+#Server for remote procedure call 
+
+from SimpleXMLRPCServer import SimpleXMLRPCServer
+
+import os
+
+
+#Enter apt ip and port no. for server
+
+server = SimpleXMLRPCServer(('172.18.39.132', 9000), logRequests=True)
+
+def add(a,b):
+
+	a=int(a)
+	b=int(b)
+	f=a+b
+	return f
+
+server.register_function(add)
+
+
+try:
+    
+	print 'Use Control-C to exit'
+    
+	server.serve_forever()
+
+except KeyboardInterrupt:
+   
+	print 'Exiting'
+
+
